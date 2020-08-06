909e51b5afc28d4a4b06c2917824ee4e6744c931
John Soneka <jsoneka1@gmail.com>
2020-08-06 11:18:00 +0200

    the Navbar template

---
+++ src/contents/navBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="top-nav">
+  
+      <router-link classs="top-link" to="/">Home</router-link>
+      <router-link classs="top-link" to="/about">About</router-link>
+      <router-link classs="top-link" to="/">Services</router-link>
+      <router-link classs="top-link" to="/about">Contacts</router-link>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped>
+.top-nav{
+    padding: 1% 10%;
+    background: #000;
+    .top-link{
+        margin: 0% 55px;
+    }
+}
+</style>
