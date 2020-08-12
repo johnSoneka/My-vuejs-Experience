73814fdc60a51f5fe20e5da29cd5183aa239b5fd
John Soneka <jsoneka1@gmail.com>
2020-08-12 09:40:45 +0200

    added the images to Assets
    added the images to Assets

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="home container-fluid">
-    <div class="row">
-      
+    <div class="row bg-success">
+      <div class="col">
+        To help reduce and eventual eradicate poverty through innovative solutions
+      </div>
+    
     </div>
-      <div class="row">
-        <h2 class="bg-danger">hello</h2>
+
+
+    <div class="row">
+      <div class="col">
+        <img src="" alt="">
+        
       </div>
     </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
+
+  
 
 
 export default {
@@ -20,3 +30,7 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+
+
+</style>
