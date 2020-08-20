7812f6fa85eadf54d9e969f4aa64dccdd72d0f45
John Soneka <jsoneka1@gmail.com>
2020-08-20 13:58:38 +0200

    using vuex

---
+++ src/store/modules/todos.js
@@ -0,0 +1,23 @@
+
+const state ={
+    todos: [
+        {
+          id: 1,
+          title: 'Todo One'
+        },
+        {
+          id: 2,
+          title: 'Todo Two'
+        }
+    ]
+};
+const getters = {};
+const mutations = {};
+const actions = {};
+
+export default{
+    state,
+    getters,
+    mutations,
+    actions
+}
